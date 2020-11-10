e365dde62d2e3658fb8ce97d1d54cff0ec8170d1
Claudio Paladini# <clode.paladini@gmail.com>
2020-11-10 09:20:17 +0000

    first commit

---
+++ app.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(plotly)
+library(tibble)
+library(dplyr)
+
+ui <- fluidPage(
+  title = "Linear Regression",
+  h1("Linear Regression"),              
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "dv",label = "Dependent Variable",choices = colnames(mtcars)),
+      selectInput(inputId = "iv",label = "Independent Variable",multiple = T,choices = colnames(mtcars)),
+      actionButton(inputId = "run",label = "Run")
+    ),
+    mainPanel(
+      textOutput(outputId = "r2"),
+      tableOutput(outputId = "result")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  get_result = eventReactive(input$run,{
+    
+    ivs = input$iv
+    dv = input$dv
+    
+    formula = as.formula(
+      paste0(
+        dv,
+        "~",
+        paste0(ivs,collapse = " + ")
+        )
+      )
+    
+    model = lm(formula = formula,data = mtcars)
+    
+  })
+  get_r2 = reactive({
+    
+    model = get_result()
+    
+    summary(model)$adj.r.squared %>% 
+      round(2)
+    
+  })
+  
+  output$result = renderTable({
+    summary(get_result())$coefficients %>% 
+      data.frame() %>% 
+      rownames_to_column("variable")
+  })
+  output$r2 = renderText({
+    paste0("Adj R2: ",get_r2())
+  })
+  
+}
+
+shinyApp(ui, server)
